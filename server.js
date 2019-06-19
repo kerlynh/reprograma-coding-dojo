4d867f2830b8e6d380e55d737970cf51efdd050b
kerlynh <kerlynh@gmail.com>
2019-06-19 20:04:41 -0300

    add data ao middleware

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const servidor = express()
 const controller = require('./PokemonsController')
 const PORT = 3000
 const logger = (request, response, next) => {
-  console.log(`Request type: ${request.method} to ${request.originalUrl}`)
+  console.log(`${new Date().toISOString()} Request type: ${request.method} to ${request.originalUrl}`)
 
   response.on('finish', () => {
     console.log(`${response.statusCode} ${response.statusMessage};`)
